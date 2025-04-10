1f628ae7dcd74841959c5f673671d95c98c7cc78
ilayda <ilydatass@gmail.com>
2025-04-10 22:25:03 +0300

    init

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,6 @@ export default {
         tailwindcss: {},
         autoprefixer: {},
     },
-}
+};
 
-// Bu sayfa, PostCSS yapılandırma dosyasıdır. PostCSS, CSS dosyalarını işlemek için kullanılan bir araçtır ve bu dosya, PostCSS eklentilerini yapılandırmak için kullanılır. Bu dosyada, Tailwind CSS ve Autoprefixer gibi eklenteler tanımlanmıştır.
+// Burada PostCSS yapılandırması yapıyoruz.
