92fb5ec9ef332bcc66b21a1bd86ce576b953fdf3
ilayda <ilydatass@gmail.com>
2025-04-11 12:19:14 +0300

    init

--- first/app.jsx
+++ first/app.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'; // Burada React ve useState'i import ediyoruz
-import StartScreen from "../components/StartScreen"; // StartScreen bileşenini import ediyoruz
+import StartScreen from "./components/StartScreen"; // StartScreen bileşenini import ediyoruz
 import QuestionScreen from './components/QuestionScreen'; // QuestionScreen bileşenini import ediyoruz
 import ResultScreen from './components/ResultScreen'; // ResultScreen bileşenini import ediyoruz
 import questions from './data/questions'; // Soruları içeren data dosyasını import ediyoruz
